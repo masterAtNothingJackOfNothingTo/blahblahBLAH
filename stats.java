cb49bd7643e5a0877d9b11cab6ef0f7b18f764d3
masterAtNothingJackOfNothingTo <luhjimyuh@gmail.com>
2025-12-11 19:24:32 -0800

    Add stats class with initial attributes

---
+++ stats.java
@@ -0,0 +1,9 @@
+public class stats {
+    double dmg = 44;
+    double pen = 24;
+    double health = 100;
+    double armor = 40;
+    double headMultiplyer = 1.5;
+    double armorHead= 14;
+    double armorLeg = 2;
+}
